80b36da9f2eca6bd2cb83af99caa358584511f66
Cris <cristhian.p.gaviria@gmail.com>
2025-02-19 11:48:40 -0500

    operaciones name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         System.out.printf("emoclew dna olleh");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("emoclew dna olleh");
+        System.out.printf("Suma");
+        System.out.printf("Resta");
+        System.out.printf("multiplicación");
+        System.out.printf("División");
 
     }
 }
